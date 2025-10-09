1f410767f2b999d51ed6a6881da0032f9a5a83df
Sharanya Prasad <sharanyaprasadbsc24@rvu.edu.in>
2025-10-09 12:24:45 +0530

    inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,20 +1,49 @@
-class Vehicle {
-    String brand = "Generic Vehicle";
-    void start() {
-        System.out.println("Vehicle is starting...");
+class Vehicle{
+    String brand;
+    int year;
+
+    Vehicle(String brand, int year){
+        this.brand = brand;
+        this.year = year;
+    }
+
+    void display(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
     }
 }
-// Subclass
-class Car extends Vehicle {
-    int wheels = 4;
-    void display() {
-        System.out.println("Brand: " + brand + ", Wheels: " + wheels);
+
+class Car extends Vehicle{
+    String model;
+
+    Car(String brand, int year, String model){
+        super(brand, year);
+        this.model = model;
+    }
+
+    void display(){
+        super.display();
+        System.out.println("Model: " + model);
     }
 }
-public class inheritance {
+
+class ElectricCar extends Car{
+    int batteryCapacity;
+
+    ElectricCar(String brand, int year, String model, int batteryCapacity){
+        super(brand, year, model);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    void display(){
+        super.display();
+        System.out.println("Battery Capacity: " + batteryCapacity + " kwh");
+    }
+}
+
+public class Lab_session_3 {
     public static void main(String[] args) {
-        Car c = new Car();
-        c.start(); // inherited from Vehicle
-        c.display(); // defined in Car
+        ElectricCar ec = new ElectricCar("Lucid", 2024, "Sapphire", 120);
+        ec.display();
     }
 }
