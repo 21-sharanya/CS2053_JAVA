a8eb899fce4152790ab45687f5c682de59e922e0
Sharanya Prasad <sharanyaprasadbsc24@rvu.edu.in>
2025-11-08 20:04:14 +0530

    labsession7.java

--- labsession7.java
+++ labsession7.java
@@ -27,7 +27,7 @@ class User implements Runnable {
     }
 }
 
-public class PrintQueueSimulation {
+public class labsession7 {
     public static void main(String[] args) {
         Printer sharedPrinter = new Printer();
 
